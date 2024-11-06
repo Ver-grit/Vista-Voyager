fb079aa68779161409728fe6176614ef2cb1c406
Ver-grit <veronicahemmings163@gmail.com>
2024-11-06 12:09:16 -0500

    Added api integration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-function generateQuote(event) {
-    event.preventDefault();
-  
-    const quoteInput = document.querySelector("input[type='text']").value;
-    const quoteText = `“The only limit to our realization of tomorrow is our doubts of today.”`;
-  
+function displayPoem(response) {
     new Typewriter("#poem", {
-      strings: [quoteText],
+      strings: response.data.answer,
       autoStart: true,
-      delay: 75,
+      delay: 1,
       cursor: "",
     });
   }
   
-  let quoteFormElement = document.querySelector("#quote-generator-form");
-  quoteFormElement.addEventListener("submit", generateQuote);
+  function generatePoem(event) {
+    event.preventDefault();
+  
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "93c52f4e02ee6o7e13660b1taa06210f";
+    let context =
+      "You are an expert quote generator. Your task is to generate a meaningful quote based on user input.";
+    let prompt = `User instructions: Generate a quote about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
   
